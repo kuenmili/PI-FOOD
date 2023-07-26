80e478fa303fb9aee1ec7e37b0b05bfb1d66418f
kuenmili <122223943+kuenmili@users.noreply.github.com>
2023-07-26 16:24:32 -0300

    changes

--- api/index.js
+++ api/index.js
@@ -27,6 +27,6 @@ const {
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
